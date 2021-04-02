ed08ca7ea1b1faa5d550a9a4a219b9dd8c1808f1
Emmanuel Otusile <otusileemmanuel@gmail.com>
2021-04-02 12:52:02 +0300

    added player, background

--- main.py
+++ main.py
@@ -1,65 +1,44 @@
 # set up the library
 import pygame
+
 pygame.init()
 
 # set up the drawing screen window
-screen_resolution = (1240, 720)
+screen_resolution = (626, 375)
 screen = pygame.display.set_mode(screen_resolution)
 
-# fill screen background with white
-screen.fill((255, 255, 255))
+# set up the background and the player
+player = pygame.image.load("player.png")
+background = pygame.image.load("background.jpg")
 
-# set FPS limit
-clock = pygame.time.Clock()
-fps = 30
-
-# All default colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GRAY = (125, 125, 125)
-LIGHT_BLUE = (64, 128, 255)
-GREEN = (0, 200, 64)
-YELLOW = (225, 225, 0)
-PINK = (230, 50, 230)
-
-
-# example of player
-coord_x, coord_y = 620, 360
-pygame.draw.circle(screen, YELLOW, (coord_x, coord_y), 50)
-direction = 0
+# set up the player movement
+x = 0
+y = 230
+speed = 0.1
 
 # running the game until the user didn't quit
 running = True
 while running:
-	# handle every event
-	for event in pygame.event.get():
 
-		# checking, whether the user quit
+	# checking, whether the user quit
+	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 
-	keys_pressed = pygame.key.get_pressed()
-	# move	
-	if keys_pressed[pygame.K_d]:
-		print('Key D pressed')
-		if direction == -3:
-			direction = 0
-		else:
-			direction = 3
-	if keys_pressed[pygame.K_a]:
-		print('Key A pressed')
-		if direction == 3:
-			direction = 0
-		else:
-			direction = -3
-	pygame.draw.circle(screen, WHITE, (coord_x, coord_y), 50)
-	coord_x += direction
-	pygame.draw.circle(screen, YELLOW, (coord_x, coord_y), 50)
+	# player movement
+	keys = pygame.key.get_pressed()
+
+	if keys[pygame.K_LEFT] and x > 0:
+		x -= speed
+	elif keys[pygame.K_RIGHT] and x < 589:
+		x += speed
+
+	# drawing the background and the player
+	screen.blit(background, (0, 0))
+	screen.blit(player, (x, y))
+
 	# flip the display
 	pygame.display.flip()
-
-	# set frames per second
-	clock.tick(fps)
 
 # Done!
 pygame.quit()
