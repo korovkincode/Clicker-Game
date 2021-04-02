82fe35f33b68ce8c647582c5e9e9cadef32fb64f
DELL <User@WIN-U2KHTL0HNOE.sl-ira.local>
2021-04-02 11:48:05 +0300

    Add frames per seconds limit

--- main.py
+++ main.py
@@ -5,21 +5,26 @@ pygame.init()
 # set up the drawing screen window
 screen_resolution = (1240, 720)
 screen = pygame.display.set_mode(screen_resolution)
+clock = pygame.time.Clock()
+fps = 60
 
 # running the game until the user didn't quit
 running = True
 while running:
 
-    # checking, whether the user quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+	# checking, whether the user quit
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
 
-    # fill screen background with purple
-    screen.fill((255, 255, 255))
+	# fill screen background with white
+	screen.fill((255, 255, 255))
 
-    # flip the display
-    pygame.display.flip()
+	# flip the display
+	pygame.display.flip()
 
+	# set frames per second
+	clock.tick(fps)
+	
 # Done!
 pygame.quit()
