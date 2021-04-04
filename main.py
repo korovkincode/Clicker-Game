2fdc10723decc7b5a772bc0cf14fe9aaaa77e291
DELL <nickorovkinwork@gmail.com>
2021-04-04 18:21:50 +0300

    Add new class Ground, add jumps

--- main.py
+++ main.py
@@ -3,17 +3,35 @@ import pygame
 pygame.init()
 
 # set up the drawing screen window
-screen_resolution = (626, 375)
+screen_resolution = [626, 375]
+player_size = [37, 57]
 screen = pygame.display.set_mode(screen_resolution)
 clock = pygame.time.Clock()
 
+class Ground:
+	def __init__(self, x, y, width):
+		self.xstart = x
+		self.xend = x + width
+		self.y = y
+
+	def isTouch(self, obj):
+		if obj.x >= self.xstart and obj.x <= self.xend and obj.y == self.y:
+			return True
+		return False
+
 class Player:
 	def __init__(self, x, y, hp = 100, speed = 3.5):
-		self.x = x
-		self.y = y
-		self.hp = hp
-		self.speed = speed
-		self.direction = 0
+		self.x = x # X position
+		self.y = y # Y position
+		self.hp = hp # Health Points
+		self.speed = speed # Speed
+		self.direction = 0 # Direction(0 -> east, 1 -> west)
+		self.jumpH = 90 # player max jump height
+		self.jumptime = 0 # air time
+		self.isFalling = False
+		self.isJumping = False
+
+ground = [Ground(0, 230, 626)]
 
 # set up the background and the player
 player_img = [pygame.image.load("static/player.png"), pygame.image.load("static/mirrored_player.png")]
@@ -36,9 +54,33 @@ while running:
 	if keys[pygame.K_LEFT] and player.x > 0:
 		player.x -= player.speed
 		player.direction = 1
-	elif keys[pygame.K_RIGHT] and player.x < 589:
+	elif keys[pygame.K_RIGHT] and player.x < screen_resolution[0] - player_size[0]:
 		player.x += player.speed
 		player.direction = 0
+	if keys[pygame.K_UP] and player.y + player.jumpH < screen_resolution[1]:
+		if not player.isJumping and not player.isFalling:
+			player.isJumping = True
+
+	# if player in jump
+	if player.isJumping:
+		if player.jumptime + 5 <= player.jumpH:
+			player.y -= 5
+			player.jumptime += 5
+		else:
+			player.jumptime = 0
+			player.isJumping = False
+			player.isFalling = True
+
+	# if player is falling
+	if player.isFalling:
+		touched = False
+		for gr in ground: # if player touches ground -> he stops falling
+			if gr.isTouch(player):
+				touched = True
+		if not touched:
+			player.y += 5
+		else:
+			player.isFalling = False
 
 	# drawing the background and the player
 	screen.blit(background_img, (0, 0))
